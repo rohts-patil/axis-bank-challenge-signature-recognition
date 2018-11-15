7210565cfd6bc250236efecf805a4302bae5a16a
rohts-patil <Rajaswa@12>
2018-11-16 00:24:33 +0530

    Remove redundant code.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 _author_ = "Rohit Patil"
 
-from flask import Flask, render_template, redirect, url_for, request
-from operations.LoginValidator import LoginValidator
+from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -21,27 +20,3 @@ login.login_view = 'login'
 
 from app.models import User
 from app import routes
-
-
-# Route for handling the welcome page logic
-#@app.route('/')
-#def welcome():
-#       return render_template('welcome.html')
-
-
-
-
-
-# Route for handling the login page logic
-#@app.route('/login', methods=['GET', 'POST'])
-def login_user():
-    error = None
-    if request.method == 'POST':
-        user = User(request.form['username'], request.form['password'])
-        validator = LoginValidator(user)
-        validator.validate()
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('welcome'))
-    return render_template('login.html', error=error)
